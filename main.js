4e84f9cf5348b528c8a2ebd05688471d2c213735
SantiagoGetial <133117838+SantiagoGetial@users.noreply.github.com>
2023-05-10 20:01:07 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+     document.querySelector('.nav-menu').classList.toggle('show'); 
+}); //Crea una clase toggle que al momento de presionar el elemento menu de la lista aparezca y desaparezca .nav-meu o la lista de mobiles 
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.novedades', {delay: 25}); 
+ScrollReveal().reveal('.cards-banner-one', {delay: 250});
+ScrollReveal().reveal('.social', {delay: 250});
+ScrollReveal().reveal('.social', {delay: 250});
+ScrollReveal().reveal('.footer-links', {delay: 250});
+ScrollReveal().reveal('.footer-container', {delay: 250});
